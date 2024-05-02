726937cc329c13c9a1db8d67db552388b50988b7
olgas <olgashyyan.uk@gmail.com>
2024-05-02 11:33:57 -0400

    Add attributes to the Employee Class

--- src/oop_practice/Employee.java
+++ src/oop_practice/Employee.java
@@ -1,6 +1,9 @@
 package oop_practice;
 
 public class Employee {
+
+    private String email;
+    private String state;
     private String id;
     private String jobTitle;
     private double salary;
