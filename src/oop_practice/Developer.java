80c46ddfdef2306b8ca41c2db3da2c190fc81c6c
olgashyyan.uk@gmail.com <olgashyyan.uk@gmail.com>
2024-05-15 07:37:01 -0400

    Generate getters and setters in Developer.java Class

--- src/oop_practice/Developer.java
+++ src/oop_practice/Developer.java
@@ -4,4 +4,20 @@ public class Developer {
 
     private String language;
     private boolean isFullTime;
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public boolean isFullTime() {
+        return isFullTime;
+    }
+
+    public void setFullTime(boolean fullTime) {
+        isFullTime = fullTime;
+    }
 }
