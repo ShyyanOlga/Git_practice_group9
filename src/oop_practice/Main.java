2504aa4516a8b3b569c4bcfd6f46eb2ac111105c
olgashyyan.uk@gmail.com <olgashyyan.uk@gmail.com>
2024-05-22 15:38:41 -0400

    Create new object in Main.java

--- src/oop_practice/Main.java
+++ src/oop_practice/Main.java
@@ -9,6 +9,9 @@ public class Main {
         Developer john = new Developer("John",42,'M',"jhondf@gmail.com","CA","W123","Developer",150.000,"Java",true);
         System.out.println(john);
 
+        Developer lina = new Developer();
+        lina.name = "Lina";
+
 
     }
 }
