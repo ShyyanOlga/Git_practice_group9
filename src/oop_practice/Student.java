0c7c7c73df7893e340ee35bfe699685fb4ae95ad
olgashyyan.uk@gmail.com <olgashyyan.uk@gmail.com>
2024-05-22 15:05:19 -0400

    Add study method to Student.java

--- src/oop_practice/Student.java
+++ src/oop_practice/Student.java
@@ -4,4 +4,7 @@ public class Student extends Person{
     public Student(String name, int age, char gender) {
         super(name, age, gender);
     }
+    public void study(){
+        System.out.println(name+ " is study");
+    }
 }
