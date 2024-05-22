55532264e30a882fe5e67d9a47c375c6bd485701
olgashyyan.uk@gmail.com <olgashyyan.uk@gmail.com>
2024-05-22 15:13:50 -0400

    Create Student and Employee object in Main.java

--- src/oop_practice/Main.java
+++ src/oop_practice/Main.java
@@ -3,5 +3,9 @@ package oop_practice;
 public class Main {
     public static void main(String[] args) {
 
+        Student lena = new Student("Lena",34,'F');
+        Employee suzy = new Employee();
+        suzy.name = "Suzy";
+
     }
 }
